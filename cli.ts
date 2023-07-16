4d8519587d32529c39201eb57e70e953ffba9485
Matias Olivera <moliverafreire@gmail.com>
2023-07-15 18:16:53 -0600

    Allow to chat via CLI

--- cli.ts
+++ cli.ts
@@ -1,3 +1,28 @@
-#!/usr/bin/env -S deno run
+#!/usr/bin/env -S deno run --allow-env --allow-net --allow-read
+
+import Ask from "https://deno.land/x/ask@1.0.6/mod.ts";
+
+import { Chat } from "./src/models/Chat.ts";
 
 console.log("Welcome to ChatGPT!");
+
+const chat = new Chat();
+
+const ask = new Ask({
+  prefix: ">",
+});
+
+while (true) {
+  const { prompt } = await ask.input({
+    name: "prompt",
+    message: "",
+  });
+
+  if (!prompt) {
+    throw new Error("No prompt provided.");
+  }
+
+  const response = await chat.sendMessage(prompt);
+
+  console.log(response);
+}
