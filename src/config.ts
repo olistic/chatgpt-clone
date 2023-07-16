684e9e1c5c5a90b889ee9b7cf275d0c28294bded
Matias Olivera <moliverafreire@gmail.com>
2023-07-16 11:08:01 -0600

    Read OpenAI API key from environment

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ import { load } from "https://deno.land/std@0.194.0/dotenv/mod.ts";
 
 const env = await load();
 
-export const OPENAI_API_KEY = env["OPENAI_API_KEY"];
+export const OPENAI_API_KEY =
+  Deno.env.get("OPENAI_API_KEY") ?? env["OPENAI_API_KEY"];
 
 export const OPENAI_BASE_URL = "https://api.openai.com/v1";
