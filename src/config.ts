3ed85dff2c9fe182e0023ccb530ee5c1d231a5e0
Matias Olivera <moliverafreire@gmail.com>
2023-07-16 17:01:02 -0700

    Simplify config loading

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,4 @@ import { load } from "dotenv";
 
 const env = await load();
 
-export const OPENAI_API_KEY =
-  Deno.env.get("OPENAI_API_KEY") ?? env["OPENAI_API_KEY"];
+export const OPENAI_API_KEY = env["OPENAI_API_KEY"];
